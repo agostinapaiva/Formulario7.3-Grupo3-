ccc0f414c467cfbb122ecad6b617e4f0b9649bb1
ana-villalba <analaura.villalba23@gmail.com>
2024-08-14 19:12:56 -0300

    Update validacion.js

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,11 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+//Validar contraseña
+if (password1.length < 6) {
+    showAlertError('La contraseña debe tener al menos 6caracteres.');
+} else if (password1 !== password2) {
+    showAlertError('Las contraseñas no coinciden.');
+} else {
+    showAlertSuccess();
+}
