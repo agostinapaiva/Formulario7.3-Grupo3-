ae23fca08cf939994c596a85e79e36c810d164ca
Gabriela Silveira <gabysole72@gmail.com>
2024-08-14 19:35:01 -0300

    Update validacion.js

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,25 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+function comparacionContrasena(password1, password2) {
+    return password1 === password2;
+}
+
+
+function verificarContrasena() {
+    let password1input = document.getElementById("password1").value;
+    let password2input = document.getElementById("password2").value;
+
+
+    if (comparacionContrasena(password1input, password2input)) {
+        alert("Las contraseñas coinciden");
+    } else {
+        alert("Las contraseñas no coinciden");
+    }
+}
+
+document.getElementById("regBtn").addEventListener("click", function() {
+    verificarContrasena();
+});
